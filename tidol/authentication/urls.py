2b9db7acfe7ccf63057dc059fd937748a5f290cc
egnaro1007 <ndviettuan@gmail.com>
2024-04-23 00:58:24 +0700

    User create author profile after authentication

--- tidol/authentication/urls.py
+++ tidol/authentication/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views import RegisterUserAPIView, CheckUsernameAPIView
+from .views import AuthorProfileAPIView
 
 urlpatterns = [
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
     path('register', RegisterUserAPIView.as_view(), name='register'),
     path('checkUsername', CheckUsernameAPIView.as_view(), name='check_username'),
+    
+    path('author-profile', AuthorProfileAPIView.as_view(), name='author_profile'),
 ]
--- tidol/authentication/views.py
+++ tidol/authentication/views.py
@@ -1,16 +1,75 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .models import CustomUser
 from .serializers import RegisterUserSerializer
+
+from bookly.models import Author
+from bookly.serializers import AuthorSerializer
 
 class RegisterUserAPIView(APIView):
     def post(self, request):
         serializer = RegisterUserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+            return Response({"message": "User register successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AuthorProfileAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        try:
+            author = Author.objects.get(user=request.user)
+        except Author.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        author_name = request.data.get('author_name')
+        if author_name is None:
+            return Response({"message": "Author name is required"}, status=status.HTTP_400_BAD_REQUEST)
+    
+        if Author.objects.filter(user=request.user).exists():
+            return Response({"message": "Author profile already exists for this user"}, status=status.HTTP_400_BAD_REQUEST)
+    
+        author = Author(name=author_name, user=request.user)
+        author.save()
+    
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
+    def put(self, request):
+        new_author_name = request.data.get('author_name')
+        if new_author_name is None:
+            return Response({"message": "New author name is required"}, status=status.HTTP_400_BAD_REQUEST)
+    
+        try:
+            author = Author.objects.get(user=request.user)
+        except Author.DoesNotExist:
+            return Response({"message": "Author profile does not exist for this user"}, status=status.HTTP_404_NOT_FOUND)
+    
+        author.name = new_author_name
+        author.save()
+    
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+     
+    def delete(self, request):
+        try:
+            author = Author.objects.get(user=request.user)
+        except Author.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+        author.delete()
+        return Response({"message": "Author profile deleted successfully"}, status=status.HTTP_200_OK)   
     
 class CheckUsernameAPIView(APIView):
     def post(self, request):
