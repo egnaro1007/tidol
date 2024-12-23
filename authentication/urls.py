2ef5b1c08217c0014454145a595d2e3b877737d8
chitrung-gg <doremob311205@gmail.com>
2024-12-24 03:10:20 +0700

    Fixing url endpoints for authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,12 +7,12 @@ from .views import AuthorProfileAPIView
 from .views import RegisterUserAPIView, CheckUsernameAPIView
 
 urlpatterns = [
-    path('whoami', WhoAmIAPIView.as_view(), name='whoami'),
-    path('token', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
-    path('register', RegisterUserAPIView.as_view(), name='register'),
-    path('checkUsername', CheckUsernameAPIView.as_view(), name='check_username'),
+    path('whoami/', WhoAmIAPIView.as_view(), name='whoami'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', RegisterUserAPIView.as_view(), name='register'),
+    path('checkUsername/', CheckUsernameAPIView.as_view(), name='check_username'),
 
-    path('author-profile', AuthorProfileAPIView.as_view(), name='author_profile'),
+    path('author-profile/', AuthorProfileAPIView.as_view(), name='author_profile'),
 ]
